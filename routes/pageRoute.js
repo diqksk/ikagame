458a60c980685db510009cc7373aba8729af3b61
diqksk <diqksk@naver.com>
2021-09-30 02:35:36 +0900

    server 60%

---
+++ routes/pageRoute.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const router = express.Router();
+const requestIp = require("request-ip");
+const { User } = require("../models");
+const { Question } = require("../models");
+const { Sequelize } = require("sequelize");
+const charaters = require("../config/character.json");
+
+router.get("/", async (req, res) => {
+  if (!req.cookies.visit) {
+    res.cookie("visit", true, {
+      maxAge: 1000 * 60 * 60 * 3,
+    });
+
+    await User.create({
+      address: requestIp.getClientIp(req),
+    });
+  }
+
+  const users = await User.findAll({
+    attributes: [
+      [Sequelize.fn("COUNT", Sequelize.col("*")), "total"],
+      [
+        Sequelize.fn("COUNT", Sequelize.literal("regdate > curdate()")),
+        "today",
+      ],
+    ],
+  });
+  let result;
+
+  result = {
+    total: users[0].dataValues.total,
+    today: users[0].dataValues.today,
+  };
+
+  res.status(200).send(result);
+});
+
+router.post("/", async (req, res) => {
+  const r = req.body;
+
+  const first_word = r.q1 + r.q2 + r.q3 > 0 ? "E" : "I";
+  const second_word = r.q4 + r.q5 + r.q6 > 0 ? "S" : "N";
+  const third_word = r.q7 + r.q8 + r.q9 > 0 ? "T" : "F";
+  const forth_word = r.q10 + r.q11 + r.q12 > 0 ? "P" : "J";
+  const mbti = first_word + second_word + third_word + forth_word;
+
+  await Question.create({ ...r, mbti: mbti });
+
+  res.status(200).send({ success: 200, msg: charaters[mbti] });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const { sequelize } = require('./models/index');
-const userRouter = require('./routes/userRoute');
+const express = require("express");
+const { sequelize } = require("./models/index");
+const userRouter = require("./routes/userRoute");
+const pageRouter = require("./routes/pageRoute");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 sequelize
   .sync()
-  .then(() => console.log('connected database'))
-  .catch(err => console.error('occurred error in database connecting', err));
+  .then(() => console.log("connected database"))
+  .catch((err) => console.error("occurred error in database connecting", err));
 
+app.use("/", userRouter);
+app.use("/ikagame", pageRouter);
 
-app.use('/api',userRouter);
-
-app.listen(3030,()=>{
-    console.log("open server on 3030");
-})
+app.listen(3060, () => {
+  console.log("open server on 3060");
+});
